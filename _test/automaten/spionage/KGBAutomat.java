31b64ab9dd2bee558dddb23662a158224180ccc1
Capetownvisitor <74378403+Capetownvisitor@users.noreply.github.com>
2024-11-07 19:13:20 +0100

    Add KGB Automat

--- _test/automaten/spionage/KGBAutomat.java
+++ _test/automaten/spionage/KGBAutomat.java
@@ -7,13 +7,46 @@ public class KGBAutomat {
 	private char[] alphabet = {'0','1','2','3','4','5','6','7','8','9'};
 	
 	public boolean imAlphabet(char pZeichen){
-		//TODO
+		for (char a: alphabet) {
+			if (a == pZeichen) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	public boolean teste(String pEingabe){
 		zustand = 0;
-		//TODO
+		for(int i = 0; i < pEingabe.length(); i++) {
+			if (!imAlphabet(pEingabe.charAt(i))) {
+				System.out.println("Character nicht im Alphabet!");
+				return false;
+			}
+
+			switch (pEingabe.charAt(i)) {
+				case '0':
+					if (zustand == 0 | zustand == 1) {
+						zustand++;
+					}
+					break;
+				case '7':
+					if(zustand == 1) {
+						zustand = 0;
+					}else if (zustand == 2) {
+						zustand++;
+					}
+					break;
+				default:
+					if(zustand == 1 || zustand == 2) {
+						zustand = 0;
+					}
+					break;
+			}
+
+			if (zustand == 3) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
